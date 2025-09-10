2c419e78e09c3d6bccb2acc495539874daa742a4
Diego <jose.rivas@uab.edu.bo>
2025-09-09 23:24:54 -0400

    Laboratorio 4 – Laravel rutas y controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaludoController;
+use App\Http\Controllers\SaludoDiegoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::get('/saludo', [SaludoController::class, 'index']);
+Route::get('/saludodiego', [SaludoDiegoController::class, 'index']);
